aca84d2f7738d411d09de9f846e26b5259d57b9d
kujenga <ataylor0123@gmail.com>
2015-06-05 16:37:42 -0400

    problem 28 solution

--- Euler_28/SpiralDiagonals.py
+++ Euler_28/SpiralDiagonals.py
@@ -1,5 +1,17 @@
 # project euler - problem 28
 # (c) 2014 Aaron Taylor
 
-size = 1001
+width = 1001
 
+total = 1
+inc = 2
+cur = 1
+n = 1
+while cur < (width**2):
+	cur += inc
+	total += cur
+	if n != 0 and n % 4 == 0:
+		inc += 2
+	n += 1
+
+print total

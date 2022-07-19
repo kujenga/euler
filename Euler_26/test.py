5e17251cdae4bdbdb070627b7689d730ed0d2079
Aaron Taylor <git@ataylor.io>
2022-07-19 18:45:01 -0400

    Euler_26: WIP solution with tests for base cases
    The found solution here of 937 does not get accepted, however.

---
+++ Euler_26/test.py
@@ -0,0 +1,27 @@
+import unittest
+from decimal import getcontext
+
+from main import str_for_frac, repeating_seq_len
+
+class TestMain(unittest.TestCase):
+
+    def test_repeating_seq_len(self):
+        # Extended precision to find cycles accurately.
+        getcontext().prec = 10000
+
+        for (i, expect) in [
+            (2, -1),
+            (3, 1),
+            (4, -1),
+            (5, -1),
+            (6, 1),
+            (7, 6),
+        ]:
+            frac = str_for_frac(i)
+            got = repeating_seq_len(frac)
+            print(frac, got)
+            self.assertEqual(got, expect, "Should match")
+
+
+if __name__ == '__main__':
+    unittest.main()

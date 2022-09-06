37279fd35045f82b6135a6ccb7a391042d43eebc
Aaron Taylor <git@ataylor.io>
2022-09-05 20:18:15 -0400

    Euler_40: solved!
    basic solution using string form of the built number

--- 031/main.py
+++ 031/main.py
@@ -1,8 +1,16 @@
 """
 https://projecteuler.net/problem=31
+
+Helper: https://www.geeksforgeeks.org/combinational-sum/
 """
 
-import itertools
+
+def combinations_sum(coins, sum):
+    results = []
+
+    # TODO: Figure out the answer!
+
+    return results
 
 
 def main():
@@ -19,24 +27,8 @@ def main():
     ]
     # Target amount is: £2 (200p)
     target_amt = 200
-    # Build a purse of the max amount of coins we could use from each type.
-    purse = []
-    max_l = 0
-    for coin in coins:
-        l = int(target_amt / coin)
-        if l > max_l:
-            max_l = l
-        purse.extend([coin] * l)
-    print('Purse:', purse)
 
-    ways = set()
-    for n in range(max_l + 1):
-        print('Testing combinations of length:', n)
-        for c in itertools.combinations(purse, n):
-            s = sum(c)
-            if s == 200 and c not in ways:
-                ways.add(c)
-                print(s, c)
+    ways = combinations_sum(coins, target_amt)
     print('Possible ways:', len(ways), ways)
 
 

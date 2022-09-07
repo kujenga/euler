204a2835e3272b8e289336dea5d8a8e066a6430c
Aaron Taylor <git@ataylor.io>
2022-09-06 20:28:13 -0400

    Euler_31: slow solution, but it works
    This took about 10 min to run. Would probably hit the "under a minute" marker in C. Could also add logic to skip out on uneeded loops to reduce time by about 1/3.

--- Euler_31/main.py
+++ Euler_31/main.py
@@ -1,35 +1,32 @@
 """
 https://projecteuler.net/problem=31
-
-Helper: https://www.geeksforgeeks.org/combinational-sum/
 """
 
 
-def combinations_sum(coins, sum):
-    results = []
-
-    # TODO: Figure out the answer!
-
-    return results
-
-
 def main():
-    # Value in pence (p)
-    coins = [
-        1,
-        2,
-        5,
-        10,
-        20,
-        50,
-        100,
-        200,
-    ]
     # Target amount is: £2 (200p)
     target_amt = 200
 
-    ways = combinations_sum(coins, target_amt)
-    print('Possible ways:', len(ways), ways)
+    def count_range(amount):
+        return range(0, int(target_amt/amount) + 1)
+
+    ways = 0
+    wealth = 0
+    for twolbs in count_range(200):
+        for onelbs in count_range(100):
+            for fifties in count_range(50):
+                for twenties in count_range(20):
+                    print(f"{ways} ways found: 200*{twolbs} + 100*{onelbs} + 50*{fifties} + 20*{twenties} ...")
+                    for tens in count_range(10):
+                        for fives in count_range(5):
+                            for twos in count_range(2):
+                                for ones in count_range(1):
+                                    wealth = twolbs*200 + onelbs*100 + fifties*50 + twenties*20 + tens*10 + fives*5 + twos*2 + ones*1
+                                    if wealth == target_amt:
+                                        ways += 1
+
+
+    print('Possible ways:', ways)
 
 
 if __name__ == '__main__':

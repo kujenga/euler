c5acd8de3f0c5f8cfc87ea96bd74c44b64ade832
Aaron Taylor <git@ataylor.io>
2022-09-08 09:06:17 -0400

    042: iterative solution + CSV parser

--- 042/Package.swift
+++ 042/Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     name: "CodedTriangleNumbers",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.8.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "CodedTriangleNumbers",
-            dependencies: []),
+            dependencies: ["SwiftCSV"]),
         .testTarget(
             name: "CodedTriangleNumbersTests",
             dependencies: ["CodedTriangleNumbers"]),

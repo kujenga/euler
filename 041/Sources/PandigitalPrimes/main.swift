276090e23212574a4bc71349dfacac08984aa8ce
Aaron Taylor <git@ataylor.io>
2022-09-08 08:22:56 -0400

    041: solution via QuickPerm
    This implements a working solution based on the linked QuickPerm approach
    to permutation, which ended up seeming to be the most straightforward
    _iterative_ solution I found (many are recursive).
    
    Also adds a "pow" helper to deal with Decimal weirdness in the lib
    package.

---
+++ 041/Sources/PandigitalPrimes/main.swift
@@ -0,0 +1,57 @@
+// https://projecteuler.net/problem=41
+
+import Euler
+
+func toInt(_ digits: [Int]) -> Int {
+    var v = 0;
+    for (index, digit) in digits.enumerated() {
+        v += digit * pow(10, index)
+    }
+    return v;
+}
+
+var largest = 0;
+
+for N in 1...9 {
+    // QuickPerm
+    // https://www.quickperm.org/
+    var a: [Int] = [];
+    // Pandigital set of digits, based on problem statement.
+    for d in 1...N {
+        a.append(d);
+    }
+    var p: [Int] = [];
+    for d in 0...N {
+        p.append(d);
+    }
+    var i = 1;
+
+    // Permute
+    while i < N {
+        // decrement p[i] by 1
+        p[i] -= 1
+        // if i is odd, then let j = p[i] otherwise let j = 0
+        let j = p[i]%2 != 0 ? p[i] : 0;
+        // swap(a[j], a[i])
+        a.swapAt(j, i)
+        // let i = 1
+        i = 1
+        // while (p[i] is equal to 0) do {
+        while p[i] == 0 {
+            // let p[i] = i
+            p[i] = i
+            // increment i by 1
+            i += 1
+        }
+
+        let x = toInt(a)
+        if x.isPrime() {
+            print("is prime: \(x)")
+            if x > largest {
+                largest = x;
+            }
+        }
+    }
+}
+
+print("largest pandigital prime: \(largest)")
